da9f9c67b7f7d4226c0ef6b152ac77281a6f2066
40745 <40745@DESKTOP-B70CVI7>
2023-10-21 14:19:48 +0300

    Add project files.

---
+++ Data/Horga_Alexandra_Lab2Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Horga_Alexandra_Lab2.Models;
+
+namespace Horga_Alexandra_Lab2.Data
+{
+    public class Horga_Alexandra_Lab2Context : DbContext
+    {
+        public Horga_Alexandra_Lab2Context (DbContextOptions<Horga_Alexandra_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Horga_Alexandra_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Horga_Alexandra_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorID);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+namespace Horga_Alexandra_Lab2.Models
+{
+    public class Book
+    {   
+        public int ID { get; set; }
+
+        [Display(Name ="Book Title")]
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price {  get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+
+        public int AuthorID { get; set; }  // Cheie străină către Authors
+
+        public Author Author { get; set; }  // Proprietate de navigare către autorul cărții
+    }
+}
