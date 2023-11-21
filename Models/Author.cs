ff0c8b8b19dbba894467bc2ddc9c58718709389a
alexandrahorga <141125808+alexandrahorga@users.noreply.github.com>
2023-11-21 17:52:26 +0200

    lab 4 (#2)
    Co-authored-by: 40745 <40745@DESKTOP-B70CVI7>

--- Models/Author.cs
+++ Models/Author.cs
@@ -9,6 +9,16 @@ namespace Horga_Alexandra_Lab2.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
         public ICollection<Book>? Books { get; set; }
     }
 }
+
